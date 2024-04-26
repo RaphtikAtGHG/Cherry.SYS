a8c1b2f948fadcc531159a9e9c1574e539310d99
Raphtik@GHG <raphael.wegmann81@gmail.com>
2024-04-26 17:21:54 +0200

    Initial Commit

---
+++ src/main/java/com/github/concrafter20/cherrysys/Cherrysys.java
@@ -0,0 +1,18 @@
+package com.github.concrafter20.cherrysys;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Cherrysys implements ModInitializer {
+
+	public static final String MOD_ID = "cherry.sys";
+
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+	}
+}
